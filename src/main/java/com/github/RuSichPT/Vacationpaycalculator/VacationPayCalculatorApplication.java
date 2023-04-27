d6c7252c3f3344f29b0c689ff585c73ab5240543
pavel.tokarev <tokarevpavel1996@mail.ru>
2023-04-27 12:38:25 +0300

    Spring boot

---
+++ src/main/java/com/github/RuSichPT/Vacationpaycalculator/VacationPayCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.RuSichPT.Vacationpaycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPayCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPayCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/RuSichPT/Vacationpaycalculator/VacationPayCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.RuSichPT.Vacationpaycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPayCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
